61b6101fa6dab82f3af84906c15a48a33b657d93
jj528s35 <jj528s35@yahoo.com.tw>
2020-01-15 22:48:55 +0800

    20200115

---
+++ Test/Assets/c#/plane_pos_rot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plane_pos_rot : MonoBehaviour
+{ 
+    public GameObject plane_;
+    public Vector3 n_vector = Vector3.up;
+    private socket_receive receive_data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        receive_data = GameObject.Find("Receive data").GetComponent<socket_receive>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float[] eq = receive_data.plane_equation; 
+        
+        n_vector = new Vector3(eq[0], eq[1], eq[2]);
+        plane_.transform.forward = n_vector*100;
+    }
+}
